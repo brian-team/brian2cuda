31cbbcf3906d52e3bb15e68a224debf551e863bb
Denis Alevi <mail@denisalevi.de>
2016-11-15 22:53:37 +0100

    Change prefs.codegen.cpp.extra_compile_args_gcc to be compatible with nvcc
    Temporary fix. Prefs need still to be adjusted for nvcc compilation
    (see #35).

--- brian2cuda/cuda_generator.py
+++ brian2cuda/cuda_generator.py
@@ -28,7 +28,7 @@ prefs.register_preferences(
         default='__restrict',
         docs='''
         The keyword used for the given compiler to declare pointers as restricted.
-        
+
         This keyword is different on different compilers, the default works for
         gcc and MSVS.
         ''',
@@ -37,7 +37,7 @@ prefs.register_preferences(
         default=False,
         docs='''
         Adds code to flush denormals to zero.
-        
+
         The code is gcc and architecture specific, so may not compile on all
         platforms. The code, for reference is::
 
@@ -45,11 +45,16 @@ prefs.register_preferences(
             unsigned csr = __builtin_ia32_stmxcsr();
             csr |= CSR_FLUSH_TO_ZERO;
             __builtin_ia32_ldmxcsr(csr);
-            
+
         Found at `<http://stackoverflow.com/questions/2487653/avoiding-denormal-values-in-c>`_.
         ''',
         ),
     )
+
+# -ffast-math, -fno-finite-math-only and -march are not compatible with nvcc
+# default = ['-w', '-O3', '-ffast-math', '-fno-finite-math-only', '-march=native']
+prefs.codegen.cpp.extra_compile_args_gcc = ['-w', '-O3']
+
 
 
 class CUDACodeGenerator(CPPCodeGenerator):
