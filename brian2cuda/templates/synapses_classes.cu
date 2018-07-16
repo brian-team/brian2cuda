fe6a9a479fe9a0d8796e424d4cfce97702c2656f
Denis Alevi <mail@denisalevi.de>
2018-07-16 03:17:26 +0200

    Add support for single precision floats

--- brian2cuda/templates/synapses_classes.cu
+++ brian2cuda/templates/synapses_classes.cu
@@ -11,10 +11,6 @@
 
 #include "brianlib/spikequeue.h"
 
-template<class scalar> class Synapses;
-template<class scalar> class SynapticPathway;
-
-template <class scalar>
 class SynapticPathway
 {
 public:
@@ -22,7 +18,6 @@ public:
     int Nsource;
     int Ntarget;
 
-    scalar* dev_delay;
     int32_t* dev_sources;
     int32_t* dev_targets;
 
@@ -31,23 +26,22 @@ public:
     int spikes_start;
     int spikes_stop;
 
-    scalar dt;
-    CudaSpikeQueue<scalar>* queue;
+    double dt;
+    CudaSpikeQueue* queue;
     bool no_or_const_delay_mode;
 
     //our real constructor
-    __device__ void init(int _Nsource, int _Ntarget, scalar* d_delay, int32_t* _sources,
-                int32_t* _targets, scalar _dt, int _spikes_start, int _spikes_stop)
+    __device__ void init(int _Nsource, int _Ntarget, int32_t* _sources,
+                int32_t* _targets, double _dt, int _spikes_start, int _spikes_stop)
     {
         Nsource = _Nsource;
         Ntarget = _Ntarget;
-        dev_delay = d_delay;
         dev_sources = _sources;
         dev_targets = _targets;
         dt = _dt;
         spikes_start = _spikes_start;
         spikes_stop = _spikes_stop;
-        queue = new CudaSpikeQueue<scalar>;
+        queue = new CudaSpikeQueue;
     };
 
     //our real destructor
@@ -57,29 +51,6 @@ public:
         delete queue;
     }
 };
-
-template <class scalar>
-class Synapses
-{
-public:
-    int _N_value;
-    inline double _N() { return _N_value;};
-    int Nsource;
-    int Ntarget;
-    std::vector< std::vector<int> > _pre_synaptic;
-    std::vector< std::vector<int> > _post_synaptic;
-
-    Synapses(int _Nsource, int _Ntarget)
-        : Nsource(_Nsource), Ntarget(_Ntarget)
-    {
-        for(int i=0; i<Nsource; i++)
-            _pre_synaptic.push_back(std::vector<int>());
-        for(int i=0; i<Ntarget; i++)
-            _post_synaptic.push_back(std::vector<int>());
-        _N_value = 0;
-    };
-};
-
 #endif
 
 {% endmacro %}
