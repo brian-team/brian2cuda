a02bd34b6c527c757a2d6c3956cc549f5c660f75
Denis Alevi <mail@denisalevi.de>
2018-07-20 11:49:37 +0200

    Modify test suite script to apply brian2 change

--- brian2cuda/tools/run_test_suite_on_current_state.sh
+++ brian2cuda/tools/run_test_suite_on_current_state.sh
@@ -52,16 +52,23 @@ source activate dev_b2c
 
 git clone "$GIT_DIR" "$WORK_DIR"
 cd "$WORK_DIR"
-git submodule update --init frozen_repos/brian2
 if [ -z "$2" ]
 then
-    echo "using tip of master"
+    echo "using tip of master" | tee -a "$LOG_FILE"
 else
-    git checkout $2
-    echo "checked out $2"
+    git checkout $2 | tee -a "$LOG_FILE"
+    #echo "checked out $2" | tee -a "$LOG_FILE"
 fi
-git rev-parse --abbrev-ref HEAD 2>&1 | tee -a "LOG_FILE"
-git rev-parse HEAD 2>&1 | tee -a "LOG_FILE"
-cd "brian2cuda/tools"
+git rev-parse --abbrev-ref HEAD 2>&1 | tee -a "$LOG_FILE"
+git rev-parse HEAD 2>&1 | tee -a "$LOG_FILE"
+git submodule update --init frozen_repos/brian2 | tee -a "$LOG_FILE"
+cd frozen_repos/brian2
+git checkout "float32_support" | tee -a "$LOG_FILE"
+#echo "Checked out brian2 version:" | tee -a "$LOG_FILE"
+git rev-parse --abbrev-ref HEAD 2>&1 | tee -a "$LOG_FILE"
+git rev-parse HEAD 2>&1 | tee -a "$LOG_FILE"
+git apply ../brian2.diff | tee -a "$LOG_FILE"
+echo "Applied brian2.diff" | tee -a "$LOG_FILE"
+cd ../../brian2cuda/tools
 PYTHONPATH="../..:../../frozen_repos/brian2:$PYTHONPATH" python run_test_suite.py --float-dtype $FLOAT_DTYPE --fail-not-implemented -j"$J" 2>&1 | tee -a "$LOG_FILE"
 
