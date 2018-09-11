a680f36a28ba76fa82ef6a49573467b6b0fc863a
Denis Alevi <mail@denisalevi.de>
2018-09-11 20:05:26 +0200

    Add test suite option to run with all prefs combinations

--- brian2cuda/tools/run_test_suite.py
+++ brian2cuda/tools/run_test_suite.py
@@ -11,6 +11,14 @@ parser.add_argument('--targets', nargs='*', default=['cuda_standalone'], type=st
 parser.add_argument('--float-dtype', nargs='*', default=['float32', 'float64'],
                     choices=['float32', 'float64'], help=("The "
                     "prefs['core.default_float_dtype'] with which tests should be run."))
+parser.add_argument('--all-prefs', action='store_true',
+                    help=("Run test suits with all preference combinations."))
+parser.add_argument('--no-bundles', action='store_true',
+                    help="Don't use bundle pushing")
+parser.add_argument('--no-atomics', action='store_true',
+                    help="Don't use atomics for effect application")
+parser.add_argument('--num-parallel-blocks', default=None, type=int, nargs=1,
+                    help="Number of parallel blocks in post neuron blocks structure.")
 parser.add_argument('--no-long-tests', action='store_false',
                     help="Set to not run long tests. By default they are run.")
 parser.add_argument('--no-reset-prefs', action='store_false',
@@ -25,6 +33,11 @@ parser.add_argument('--jobs', '-j', nargs=1, type=int, default=[12],
                     help="Number of cores to use for compilations, passed as -j option "
                          "to the make commands.")
 args = parser.parse_args()
+
+single_prefs_set = any([args.no_atomics, args.no_bundles,
+                        args.num_parallel_blocks is not None])
+assert not (args.all_prefs and single_prefs_set), \
+        "Can't set other preferences in combination with --all-prefs"
 
 import os
 from StringIO import StringIO
@@ -45,9 +58,40 @@ prefs['codegen.cuda.extra_compile_args_nvcc'].extend(['-Xcudafe "--diag_suppress
 
 prefs['devices.cpp_standalone.extra_make_args_unix'] = ['-j' + str(args.jobs[0])]
 
+if args.all_prefs:
+    # create a powerset (all combinations) of the all_prefs_list
+
+    from itertools import chain, combinations
+
+    def powerset(iterable):
+        "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
+        s = list(iterable)
+        return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
+
+    all_prefs_list = [{'devices.cuda_standalone.parallel_blocks': 1},
+                      {'devices.cuda_standalone.push_synapse_bundles': False},
+                      {'codegen.generators.cuda.use_atomics': False}]
+
+    all_prefs_combinations = []
+    for dict_tuple in powerset(all_prefs_list):
+        all_prefs_combinations.append(dict(item for d in dict_tuple for item in d.items()))
+    print "Running test suite for following prefs combinations:\n"
+    for n, d in enumerate(all_prefs_combinations):
+        print n, ":", d, "\n" 
+
+else:
+    # just set the preferences chosen
+    all_prefs_combinations = [None]
+    if args.no_atomics:
+        prefs['codegen.generators.cuda.use_atomics'] = False
+
+    if args.no_bundles:
+        prefs['devices.cuda_standalone.push_synapse_bundles'] = False
+
+    if args.num_parallel_blocks is not None:
+        prefs['devices.cuda_standalone.parallel_blocks'] = args.num_parallel_blocks[0]
+
 if socket.gethostname() == 'elnath':
-    if prefs['devices.cpp_standalone.extra_make_args_unix'] == ['-j12']:
-        prefs['devices.cpp_standalone.extra_make_args_unix'] = ['-j24']
     prefs['codegen.cuda.extra_compile_args_nvcc'].remove('-arch=sm_35')
     prefs['codegen.cuda.extra_compile_args_nvcc'].extend(['-arch=sm_20'])
 
@@ -70,17 +114,30 @@ for target in args.targets:
     test_in_parallel = []
     if target in args.test_parallel:
         test_in_parallel = [target]
+    if target == 'cuda_standalone':
+        preference_dictionaries = all_prefs_combinations
+    else:
+        preference_dictionaries = [None]
 
     for dtype in dtypes:
-        test(codegen_targets=[],
-             long_tests=args.no_long_tests,
-             test_codegen_independent=False,
-             test_standalone=target,
-             reset_preferences=False,
-             fail_for_not_implemented=args.fail_not_implemented,
-             test_in_parallel=test_in_parallel,
-             extra_test_dirs=extra_test_dirs,
-             float_dtype=dtype
-            )
+        for prefs_dict in preference_dictionaries:
 
-        prefs.read_preference_file(StringIO(stored_prefs))
+            # reset prefs to stored prefs
+            prefs.read_preference_file(StringIO(stored_prefs))
+
+            if prefs_dict is not None:
+                print "\nRUNNING test suite on CUDA_STANDALONE with prefs:"
+                for k, v in prefs_dict:
+                    prefs[k] = v
+                    print "\tprefs[{}] = {}".format(k,v)
+
+            test(codegen_targets=[],
+                 long_tests=args.no_long_tests,
+                 test_codegen_independent=False,
+                 test_standalone=target,
+                 reset_preferences=False,
+                 fail_for_not_implemented=args.fail_not_implemented,
+                 test_in_parallel=test_in_parallel,
+                 extra_test_dirs=extra_test_dirs,
+                 float_dtype=dtype
+                )
---
+++ brian2cuda/tools/run_test_suite_on_current_state_all_prefs.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+# arguments:
+# $1: additional name for log_file
+# $2: commit hash or branch name to check out after cloning
+# $3: float32 or float64 or both (default)
+# $4: number of cores used for parallel compilation (make -j $4)
+
+# the directory of the script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# the temp directory used, within $DIR
+WORK_DIR=`mktemp -d -p "$DIR"`
+# the directory of the brian2cuda repository
+GIT_DIR="$(dirname "$(dirname "`pwd`")")"
+# the file for loggint the output
+TIME=`date +"%y.%m.%d_%H:%M"`
+LOG_FILE="$DIR/logfile_tests_$1_$TIME.txt"
+
+# deletes the temp directory
+function cleanup {
+    rm -rf "$WORK_DIR"
+    echo "Deleted temp working directory $WORK_DIR"
+}
+
+# register the cleanup function to be called on the EXIT signal
+trap cleanup EXIT
+
+if [ -z "$4" ]
+then
+    J=""
+else
+    J="-j$4"
+fi
+
+if [ -z "$3" ]
+then
+    FLOAT_DTYPE="float64 float32"
+elif [ "$3" = "float32" ]
+then
+    FLOAT_DTYPE="float32"
+elif [ "$3" = "float64" ]
+then
+    FLOAT_DTYPE="float64"
+elif [ "$3" = "both" ]
+then
+    FLOAT_DTYPE="float64 float32"
+else
+    echo "ERROR, the third argument needs to be from {'float32'|'float64'|'both'}"
+    exit 1
+fi
+
+source activate dev_b2c
+
+git clone "$GIT_DIR" "$WORK_DIR"
+cd "$WORK_DIR"
+if [ -z "$2" ]
+then
+    echo "using tip of master" | tee -a "$LOG_FILE"
+else
+    git checkout $2 | tee -a "$LOG_FILE"
+    #echo "checked out $2" | tee -a "$LOG_FILE"
+fi
+git rev-parse --abbrev-ref HEAD 2>&1 | tee -a "$LOG_FILE"
+git rev-parse HEAD 2>&1 | tee -a "$LOG_FILE"
+git submodule update --init frozen_repos/brian2 | tee -a "$LOG_FILE"
+cd frozen_repos/brian2
+#git checkout "float32_support" | tee -a "$LOG_FILE"
+#echo "Checked out brian2 version:" | tee -a "$LOG_FILE"
+git rev-parse --abbrev-ref HEAD 2>&1 | tee -a "$LOG_FILE"
+git rev-parse HEAD 2>&1 | tee -a "$LOG_FILE"
+git apply ../brian2.diff | tee -a "$LOG_FILE"
+echo "Applied brian2.diff" | tee -a "$LOG_FILE"
+cd ../../brian2cuda/tools
+cmd=python run_test_suite.py --float-dtype $FLOAT_DTYPE --fail-not-implemented "$J" --all-prefs -- 2>&1 | tee -a "$LOG_FILE"
+echo $cmd
+PYTHONPATH="../..:../../frozen_repos/brian2:$PYTHONPATH" $cmd
