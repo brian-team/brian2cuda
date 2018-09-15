bc46e9380b723f8cb024483cc77d73c228c67ee9
Denis Alevi <mail@denisalevi.de>
2018-09-15 18:30:47 +0200

    Update timestep function following brian2 change
    See PR brian-team/brian2#996

--- brian2cuda/cuda_generator.py
+++ brian2cuda/cuda_generator.py
@@ -805,16 +805,7 @@ namespace std {
 __host__ __device__
 static inline int _timestep(double t, double dt)
 {
-    // maximum 32bit integer divided by 2
-    const int _infinity_int = 1073741823;
-    if (std::isinf(t))
-    {
-        if (t < 0)
-            return -_infinity_int;
-        else
-            return _infinity_int;
-    }
-    return (int)((t + 1e-3*dt)/dt);
+    return (int64_t)((t + 1e-3*dt)/dt);
 }
 '''
 DEFAULT_FUNCTIONS['timestep'].implementations.add_implementation(CUDACodeGenerator,
