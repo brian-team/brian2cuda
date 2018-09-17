d2c20ae2609111b2ba5a8e490b94351e00e9d62f
Denis Alevi <mail@denisalevi.de>
2018-09-17 23:44:07 +0200

    Add bash script to run all examples

---
+++ examples/run_examples.sh
@@ -0,0 +1,68 @@
+# example script to run multiple examples, the code and results will be in:
+base='benchmark_consistency_test'
+codefolder=$base/code
+resultsfolder=$base/results
+lofgile=$base/log.txt
+
+mkdir $base
+for n in 5000 50000 500000; do
+    for a_flag in "" "no-"; do
+        for b_flag in "" "no-"; do
+            for sp_flag in "" "no-"; do
+                for bl in 1 15; do
+
+                    args="--devicename cuda_standalone \
+                        --resultsfolder $resultsfolder \
+                        --codefolder $codefolder \
+                        --profiling \
+                        --monitors \
+                        --N $n \
+                        --"$a_flag"atomics \
+                        --"$b_flag"bundle-mode \
+                        --"$sp_flag"single-precision \
+                        --num-blocks $bl"
+
+                    cmd="python brunelhakim.py --no-heterog-delays $args"
+                    echo $cmd
+                    $cmd 2>&1 | tee -a $logfile
+
+                    cmd="python brunelhakim.py --heterog-delays --narrow-delaydistr $args"
+                    echo $cmd
+                    $cmd 2>&1 | tee -a $logfile
+
+                    cmd="python brunelhakim.py --heterog-delays --no-narrow-delaydistr $args"
+                    echo $cmd
+                    $cmd 2>&1 | tee -a $logfile
+
+                    cmd="python cobahh.py --scenario brian2-example $args"
+                    echo $cmd
+                    $cmd 2>&1 | tee -a $logfile
+
+                    cmd="python cobahh.py --scenario pseudocoupled-1000 $args"
+                    echo $cmd
+                    $cmd 2>&1 | tee -a $logfile
+
+                    cmd="python cobahh.py --scenario pseudocoupled-80 $args"
+                    echo $cmd
+                    $cmd 2>&1 | tee -a $logfile
+
+                    cmd="python cobahh.py --scenario uncoupled $args"
+                    echo $cmd
+                    $cmd 2>&1 | tee -a $logfile
+
+                    cmd="python mushroombody.py $args"
+                    echo $cmd
+                    $cmd 2>&1 | tee -a $logfile
+
+                    cmd="python cuba.py $args"
+                    echo $cmd
+                    $cmd 2>&1 | tee -a $logfile
+
+                    cmd="python stdp.py $args"
+                    echo $cmd
+                    $cmd 2>&1 | tee -a $logfile
+                done
+            done
+        done
+    done
+done
