2019aca102c9b9b2bcb9aad1dcd662ebe74b66b0
Denis Alevi <mail@denisalevi.de>
2018-10-11 18:41:53 +0200

    Fix packages imports in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='brian2cuda',
     version='dev',
-    packages=['brian2cuda',],
+    packages=find_packages(),
 )
