803edb13c74875b57e06b4b84a82caf284e0c0a8
Denis Alevi <mail@denisalevi.de>
2018-10-11 18:58:04 +0200

    Copy template files in setup.py

--- setup.py
+++ setup.py
@@ -4,4 +4,12 @@ setup(
     name='brian2cuda',
     version='dev',
     packages=find_packages(),
+    package_data={# include template files
+                  'brian2cuda': ['templates/*.cu',
+                                 'templates/*.h',
+                                 'templates/makefile',
+                                 'templates/win_makefile',
+                                 'brianlib/*.cu',
+                                 'brianlib/*.h'],
+                 },
 )
