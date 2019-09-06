6c848c935b59fca0bb6a101812fd8971c578b895
Denis Alevi <mail@denisalevi.de>
2019-09-06 13:43:28 +0200

    Adapt test suite scripts

--- brian2cuda/tools/run_test_suite_on_current_state.sh
+++ brian2cuda/tools/run_test_suite_on_current_state.sh
@@ -7,13 +7,16 @@
 
 # the directory of the script
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# folder name for log files (relative to $DIR)
+LOG_DIR=test_suit_logs
 # the temp directory used, within $DIR
 WORK_DIR=`mktemp -d -p "$DIR"`
 # the directory of the brian2cuda repository
 GIT_DIR="$(dirname "$(dirname "`pwd`")")"
-# the file for loggint the output
-TIME=`date +"%y.%m.%d_%H:%M"`
-LOG_FILE="$DIR/logfile_tests_$1_$TIME.txt"
+# the file for logging the output
+LOG_FILE="$DIR/$LOG_DIR/$(date +%y-%m-%d_%T)_$1"
+
+mkdir -p "$DIR/$LOG_DIR"
 
 # deletes the temp directory
 function cleanup {
@@ -31,24 +34,32 @@ else
     J=$4
 fi
 
+# single and double precision flags
+SP=false
+DP=false
+
 if [ -z "$3" ]
 then
-    FLOAT_DTYPE="float64 float32"
+    SP=true
+    DP=true
 elif [ "$3" = "float32" ]
 then
-    FLOAT_DTYPE="float32"
+    SP=true
 elif [ "$3" = "float64" ]
 then
-    FLOAT_DTYPE="float64"
+    DP=true
 elif [ "$3" = "both" ]
 then
-    FLOAT_DTYPE="float64 float32"
+    SP=true
+    DP=true
 else
     echo "ERROR, the third argument needs to be from {'float32'|'float64'|'both'}"
     exit 1
 fi
 
-source activate dev_b2c
+source /etc/profile.d/conda.sh
+export CONDA_EXE=/usr/bin/conda
+conda activate b2c
 
 git clone "$GIT_DIR" "$WORK_DIR"
 cd "$WORK_DIR"
@@ -70,5 +81,18 @@ git rev-parse HEAD 2>&1 | tee -a "$LOG_FILE"
 git apply ../brian2.diff | tee -a "$LOG_FILE"
 echo "Applied brian2.diff" | tee -a "$LOG_FILE"
 cd ../../brian2cuda/tools
-PYTHONPATH="../..:../../frozen_repos/brian2:$PYTHONPATH" python run_test_suite.py --float-dtype $FLOAT_DTYPE --fail-not-implemented -j"$J" 2>&1 | tee -a "$LOG_FILE"
+
+if [ "$SP" = true ]
+then
+    echo "\n\n\nRunning tests in single precision mode..." | tee -a "$LOG_FILE"
+    PYTHONPATH="../..:../../frozen_repos/brian2:$PYTHONPATH" python run_test_suite.py --single-precision --fail-not-implemented -j"$J" 2>&1 | tee -a "$LOG_FILE"
+    echo "\n... Done with tests in single precision mode."
+fi
+
+if [ "$DP" = true ]
+then
+    echo "\n\n\nRunning tests in double precision mode..." | tee -a "$LOG_FILE"
+    PYTHONPATH="../..:../../frozen_repos/brian2:$PYTHONPATH" python run_test_suite.py --fail-not-implemented -j"$J" 2>&1 | tee -a "$LOG_FILE"
+    echo "\n... Done with tests in double precision mode."
+fi
 
