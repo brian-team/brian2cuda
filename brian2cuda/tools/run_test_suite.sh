49679656bd47851c7c9bc27238c2b25782827e96
Denis Alevi <mail@denisalevi.de>
2020-01-22 20:11:03 +0100

    Implement test suite script for remote grid engine qsub

--- brian2cuda/tools/run_test_suite.sh
+++ brian2cuda/tools/run_test_suite.sh
@@ -1,4 +1,11 @@
-logdir=test_suit_logs
-logfile="$logdir/$(date +%y-%m-%d_%T)"
-mkdir -p "$logdir"
-python run_test_suite.py 2>&1 | tee $logfile
+# Run test suite with current repository (sets PYTHONPATH).
+# $1 task name (optional, default: noname)
+# $2 logdir (optional, default: test_suit_logs)
+
+task_name=${1:-noname}       # default: noname
+logdir=${2:-test_suit_logs}  # default: test_suit_logs
+
+# add timestemp to name
+task_name="$(date +%y-%m-%d_%T)"__"$task_name"
+
+bash _run_test_suite.sh "$task_name" "$logdir"
