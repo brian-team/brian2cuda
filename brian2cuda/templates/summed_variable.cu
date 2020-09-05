afa3ad7cf12a476587cf7404e883240122a7d55a
Denis Alevi <mail@denisalevi.de>
2020-09-05 20:32:21 +0200

    Use cudaMemset instead of thrust::fill to reset summed variables
    Better performance (eyeballed and assumed). Not 100% sure if this works
    for all CUDA GPUs. Asked on NVIDIA forum:
    https://forums.developer.nvidia.com/t/can-i-set-a-floats-to-zero-with-cudamemset/153706

--- brian2cuda/templates/summed_variable.cu
+++ brian2cuda/templates/summed_variable.cu
@@ -1,25 +1,17 @@
 {% extends 'common_group.cu' %}
 
 
-{% block extra_headers %}
-#include <thrust/fill.h>
-{% endblock %}
-
-
 {% block extra_kernel_call %}
 {# Get the thrust vector variable name with `access_data=False` #}
 {% set _target_var_array_host = get_array_name(_target_var, access_data=False) %}
 {% set _target_var_dtype = c_data_type(_target_var.dtype) %}
 {# `constant_or_scalar` enables summed variables for connections to a synapse #}
 const int _target_size = {{constant_or_scalar(_target_size_name, variables[_target_size_name])}};
-// thrust functions interpret normal pointers as host pointers
-// -> wrap pointer in thrust::device_ptr
-thrust::device_ptr<{{_target_var_dtype}}> _target_var_thrust_ptr(dev{{_target_var_array_host}});
 // Reset summed variables to zero
-THRUST_CHECK_ERROR(
-        thrust::fill_n(_target_var_thrust_ptr + {{_target_start}},
-                       _target_size,
-                       0)
+CUDA_SAFE_CALL(
+        cudaMemset(dev{{_target_var_array_host}} + {{_target_start}},
+                   0,
+                   _target_size * sizeof({{_target_var_dtype}}))
         );
 {% endblock extra_kernel_call %}
 
