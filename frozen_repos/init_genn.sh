41605d7f5475686d085794e2878e26f7d6748194
Denis Alevi <mail@denisalevi.de>
2021-07-13 16:37:43 +0200

    Print if GENN_PATH or CUDA_PATH are modified

--- frozen_repos/init_genn.sh
+++ frozen_repos/init_genn.sh
@@ -6,7 +6,9 @@
 # If CUDA_PATH is not set, set it to /usr/local/cuda
 if [[ -z "${CUDA_PATH}" ]]; then
     export CUDA_PATH=/usr/local/cuda
+    echo "Setting CUDA_PATH=$CUDA_PATH"
 fi
 
 export GENN_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )/genn" && pwd )"
+echo "Setting GENN_PATH=$GENN_PATH"
 export PATH=$PATH:$GENN_PATH/lib/bin
