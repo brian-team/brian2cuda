1f08f49d2dfa66bf2e776a3eede5829bc51cd295
Denis Alevi <mail@denisalevi.de>
2021-08-02 14:11:04 +0200

    Add run_regularly test from brian2 tests
    Brian2 tests is marked as `codegen-independent`. We now have a CUDA
    version. Should probably instead be `standalone-compatible` in brian2
    instead, see #183.

---
+++ brian2cuda/tests/test_network.py
@@ -0,0 +1,41 @@
+from nose import with_setup
+from nose.plugins.attrib import attr
+from numpy.testing.utils import assert_equal, assert_raises
+
+from brian2 import *
+from brian2.tests.utils import assert_allclose
+from brian2.devices.device import reinit_and_delete
+
+import brian2cuda
+
+
+# Copied from brian2.tests.test_network:test_both_equal (which is 'codegen-independent')
+@attr('standalone-compatible', 'multiple-runs')
+@with_setup(teardown=reinit_and_delete)
+def test_both_equal():
+
+    set_device('cuda_standalone', directory=None, build_on_run=False)
+    #check all objects added by Network.add() also have their contained_objects added to 'Network'
+    tau = 10*ms
+    diff_eqn='''dv/dt = (1-v)/tau : 1'''
+    chg_code='''v = 2*v'''
+
+    Ng = NeuronGroup(1,diff_eqn,method='exact')
+    M1 = StateMonitor(Ng, 'v', record=True)
+    netObj = Network(Ng,M1)
+    Ng.run_regularly(chg_code, dt=20*ms)
+    netObj.run(100*ms)
+
+    start_scope()
+    Ng = NeuronGroup(1,diff_eqn,method='exact')
+    M2 = StateMonitor(Ng, 'v', record=True)
+    Ng.run_regularly(chg_code, dt=20*ms)
+    run(100*ms)
+
+    device.build(direct_call=False, **device.build_options)
+
+    assert (M1.v == M2.v).all()
+
+
+if __name__ == '__main__':
+    test_both_equal()
