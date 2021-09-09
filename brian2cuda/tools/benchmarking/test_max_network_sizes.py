a4eea8fd3402efbde556d5af7547dfff4dc0aa50
Denis Alevi <mail@denisalevi.de>
2021-09-09 10:31:08 +0200

    Update max net script

--- brian2cuda/tools/benchmarking/test_max_network_sizes.py
+++ brian2cuda/tools/benchmarking/test_max_network_sizes.py
@@ -4,6 +4,8 @@ matplotlib.use('Agg')
 
 import time
 import datetime
+import os
+import socket
 
 from brian2 import *
 from brian2.tests.features import *
@@ -13,6 +15,21 @@ from brian2.tests.features.base import results
 import brian2cuda
 from brian2cuda.tests.features.cuda_configuration import CUDAStandaloneConfiguration
 from brian2cuda.tests.features.speed import *
+
+bot = None
+try:
+    from clusterbot import ClusterBot
+except ImportError:
+    print("WARNING: clusterbot not installed. Can't notify slack.")
+else:
+    try:
+        bot = ClusterBot()
+    except Exception:
+        print(
+            f"ERROR: ClusterBot failed to initialize correctly. Can't notify "
+            f"slack. Here is the error traceback:\n{traceback.format_exc()}"
+        )
+        bot = None
 
 
 speed_tests = [# feature_test                     name                                  n_slice
@@ -59,56 +76,111 @@ speed_tests = [# feature_test                     name                          
                #(COBAHHFixedConnectivity,                        'COBAHHFixedConnectivity',                      slice(None)         ),
 ]
 
-prefs_dict = {}
-#prefs_dict = {"devices.cuda_standalone.cuda_backend.compute_capability": 7.5}
+slack_thread = None
+script = os.path.basename(__file__)
+host = socket.gethostname()
+start_msg = f"RUNNING `{script}` on {host}"
+if bot is not None:
+    try:
+        slack_thread = bot.message(start_msg)
+        bot.init_pbar(max_value=len(speed_tests), reply_to=slack_thread)
+    except Exception as exc:
+        print_flushed(f"Failed to init slack notifications, no slack messages will be sent\n{exc}")
+        bot = None
+
+print_flushed(start_msg, slack=False)
+
+
+def print_flushed(string, slack=True):
+    if slack and bot is not None:
+        bot.reply(slack_thread, string)
+    print(string, flush=True)
+
+
 time_format = '%d.%m.%Y at %H:%M:%S'
 for (ft, name, sl) in speed_tests:
     codes = []
-    # Example: ft.n_range = [10, 100, 1000, 10000]
-    length = len(ft.n_range)
-    # Example: with sl = slice(None) -> sl_idcs = (0, 4, 1)
-    sl_idcs = sl.indices(length)
-    # Example: max_idx = 4 - 1 = 3 (last index in n_range)
-    max_idx = sl_idcs[1] - sl_idcs[2]
-    # Example: n_init = 10000
-    n_init = ft.n_range[max_idx]
+    know_last_fail = False
+    break_after_first = False
+    if isinstance(sl, slice):
+        # Example: ft.n_range = [10, 100, 1000, 10000]
+        length = len(ft.n_range)
+        # Example: with sl = slice(None) -> sl_idcs = (0, 4, 1)
+        sl_idcs = sl.indices(length)
+        # Example: max_idx = 4 - 1 = 3 (last index in n_range)
+        max_idx = sl_idcs[1] - sl_idcs[2]
+        # Example: n_init = 10000
+        #n_init = ft.n_range[max_idx]
+        n_init = ft.n_range[-1] * 2
+    else:
+        if len(sl) == 1:
+            # Only test one n
+            n_init = 1
+            break_after_first = True
+        # sl is (last_success, last_fail)
+        assert len(sl) == 2
+        n_last_success = sl[0]
+        n_last_fail = sl[1]
+        n_init = int(n_last_success + (n_last_fail - n_last_success) / 2)
+        know_last_fail = True
     n = n_prev = n_init
     #n = ft.n_range[0]
     has_failed = False
-    for i in range(10):
+    for i in range(3):
         if name.startswith('STDPCUDA'):
             n = (n//1000) * 1000
         start = time.time()
         script_start = datetime.datetime.fromtimestamp(start).strftime(time_format)
-        print(f"LOG RUNNING {name} with n={n} (start at {script_start})")
-        tb, res, runtime, prof_info = results(CUDAStandaloneConfiguration, ft, n, prefs_dict=prefs_dict)
-        print(f"LOG FINISHED {name} with n={n} in {time.time() - start:.2f}s")
+        print_flushed(f"LOG RUNNING {name} with n={n} (start at {script_start})")
+        tb, res, runtime, prof_info = results(CUDAStandaloneConfiguration, ft, n)
+        print_flushed(f"LOG FINISHED {name} with n={n} in {time.time() - start:.2f}s")
         codes.append((n, res, tb))
         diff = np.abs(n - n_prev)
         n_prev = n
-        if isinstance(res, Exception):
-            print("LOG FAILED:\n", res, tb)
-            has_failed = True
-            if i == 0:
-                #print(f"LOG FAILED at first run ({name}) n={n}\n\terror={res}\nt\ttb={tb}")
-                print(f"LOG FAILED at first run ({name}) n={n}\n\terror={res}\nTraceback printed above.")
-                break
-            # assuming the first run passes, when we fail we always go down half the abs distance
-            n -= int(0.5 * diff)
-            print("LOG HALF DOWN")
-        elif has_failed:
-            n += int(0.5 * diff)
-            print("LOG HALF UP")
-        else:  # not has_failed
-            n *= 2
-            print("LOG DOUBLE")
-        if i != 0 and diff <= 10000:
-            print(f"LOG BREAK {name} after n={n} (diff={diff})")
+        if not know_last_fail:
+            if isinstance(res, Exception):
+                print_flushed(f"LOG FAILED:\n{res}, {tb}")
+                if i == 0:
+                    print_flushed(f"LOG FAILED at first run ({name}) n={n}\n\terror={res}\nt\ttb={tb}")
+                    #print_flushed(f"LOG FAILED at first run ({name}) n={n}\n\terror={res}\nTraceback printed above.")
+                    break
+                # assuming the first run passes, when we fail we always go down half the abs distance
+                n -= int(0.5 * diff)
+                print_flushed("LOG HALF DOWN")
+            elif has_failed:
+                n += int(0.5 * diff)
+                print_flushed("LOG HALF UP")
+            else:  # not has_failed
+                n *= 2
+                print_flushed("LOG DOUBLE")
+        else:  # know_last_fail = True
+            # we have (last_success, last_fail) numbers, choose always half between last success and last fail
+            if isinstance(res, Exception):
+                print_flushed(f"LOG FAILED:\n{res}, {tb}")
+                if n < n_last_fail:
+                    n_last_fail = n
+                print_flushed("LOG HALF DOWN TO LAST SUCCESS")
+            else:  # passed
+                if n > n_last_success:
+                    n_last_success = n
+                print_flushed("LOG HALF UP TO NEXT FAIL")
+            n = int(n_last_success + (n_last_fail - n_last_success) / 2)
+                    
+        if break_after_first or (i != 0 and diff <= 10000):
+            print_flushed(f"LOG BREAK {name} after n={n} (diff={diff})")
             break
-    print(f"LOG FINAL RESULTS FOR {name}:")
+    print_flushed(f"LOG FINAL RESULTS FOR {name}:")
     for n, res, tb in codes:
         if isinstance(res, Exception):
             sym = 'ERROR'
         else:
             sym = 'PASS'
-        print(f"\t{n}\t\t{sym}")
+        print_flushed(f"\t{n}\t\t{sym}")
+
+    if bot is not None:
+        bot.update_pbar()
+
+final_msg = "FINISHED `{script}` on {host} in {total_time}"
+if bot is not None:
+    bot.update(slack_thread, final_msg)
+print_flushed(final_msg, slack=False)
