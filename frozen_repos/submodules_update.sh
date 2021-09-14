fad550a0d7ec2f85462bf22b53fc534a0bfb853e
Denis Alevi <mail@denisalevi.de>
2021-09-14 11:15:57 +0200

    Make submodule init script more robust

--- frozen_repos/submodules_update.sh
+++ frozen_repos/submodules_update.sh
@@ -1,28 +1,33 @@
 #!/bin/bash
 # Initialize all submodules and update them with diff files if they exist
 
-for submodule in brian2 brian2genn genn; do
-
-    if [ -n "$(ls -A $submodule)" ]; then
-        read -p "frozen_repos/$submodule is not empty. Do you want to delete and reinitialize it? [Y/n]" answer
-        if [ $answer = "Y" ]; then
-            echo "Deleting frozen_repos/$submodule ..."
-            rm -rf $submodule
-            mkdir $submodule
-        else
-            echo "Leaving frozen_repos/$submodule unchanged"
-            continue
+(
+    # cd into git root directory, needed for sumbodule update --init
+    cd $(git rev-parse --show-toplevel)
+    for sub in brian2 brian2genn genn; do
+   	submodule=frozen_repos/"$sub"
+        if [ -n "$(ls -A $submodule)" ]; then
+            read -p "$submodule is not empty. Do you want to delete and reinitialize it? [Y/n]" answer
+            if [ $answer = "Y" ]; then
+                echo "Deleting $submodule ..."
+                rm -rf $submodule
+                mkdir $submodule
+            else
+                echo "Leaving $submodule unchanged"
+                continue
+            fi
         fi
-    fi
-
-    echo "Updating frozen_repos/$submodule ..."
-    git submodule update --init $submodule
-    cd $submodule >/dev/null || (echo "ERROR: 'cd $submodule' failed, exiting" && exit)
-    if [ -f ../$submodule.diff ]; then
-        echo "Applying $submodule.diff ..."
-        git apply -v ../$submodule.diff
-    else
-        echo "No diff file found for $submodule"
-    fi
-    cd - >/dev/null || (echo "ERROR: 'cd -' failed, exiting" && exit)
-done
+    
+        echo "Updating $submodule ..."
+        git submodule update --init $submodule
+        (
+            cd $submodule
+            if [ -f ../$sub.diff ]; then
+                echo "Applying $sub.diff ..."
+                git apply -v ../$sub.diff
+            else
+                echo "No diff file found for $submodule"
+            fi
+        )
+    done
+)
