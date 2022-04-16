3c875045b757abdf545a1fecf22c16bcf49e0c48
Denis Alevi <mail@denisalevi.de>
2022-04-16 16:38:53 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,57 @@
+from pathlib import Path
 from setuptools import setup, find_packages
 
+# Use readme file as long description
+readme_file = Path(__file__).parent / "README.md"
+long_description = readme_file.read_text()
+
 setup(
-    name='brian2cuda',
-    version='dev',
+    name="brian2cuda",
+    version="0.1",
     packages=find_packages(),
-    package_data={# include template files
-                  'brian2cuda': ['templates/*.cu',
-                                 'templates/*.h',
-                                 'templates/makefile',
-                                 'templates/win_makefile',
-                                 'brianlib/*.cu',
-                                 'brianlib/*.h'],
-                 },
-    install_requires=['brian2>=2.4.2'],
+    package_data={
+        # include template files
+        "brian2cuda": [
+            "templates/*.cu",
+            "templates/*.h",
+            "templates/makefile",
+            "templates/win_makefile",
+            "brianlib/*.cu",
+            "brianlib/*.h",
+        ],
+        # Include external code for tests
+        "brian2cuda.tests": [
+            "func_def_cuda.cpp",
+            "func_def_cuda.h",
+        ],
+    },
+    install_requires=["brian2==2.4.2"],
+    provides=["brian2cuda"],
+    extras_require={
+        "test": ["pytest", "pytest-xdist>=1.22.3"],
+        "docs": ["sphinx>=1.8"],
+    },
+    url="http://github.com/brian-team/brian2cuda",
+    project_urls={
+        "Documentation": "https://brian2cuda.readthedocs.io/en/latest/",
+        "Bug tracker": "https://github.com/brian-team/brian2cuda/issues",
+    },
+    description="A Brian2 extention to simulate spiking neural networks on GPUs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Denis Alevi, Moritz Augustin and Marcel Stimberg",
+    author_email="mail@denisalevi.de",
+    keywords="computational neuroscience simulation",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Environment :: GPU :: NVIDIA CUDA",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+    ],
+    python_requires=">=3.6",
 )
