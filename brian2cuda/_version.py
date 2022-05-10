c7cea53c18f812a37cbcafc190dd61b5ae79a326
Marcel Stimberg <marcel.stimberg@inserm.fr>
2022-05-10 10:52:59 +0200

    Only consider tags starting with "v" as releases

--- brian2cuda/_version.py
+++ brian2cuda/_version.py
@@ -43,7 +43,7 @@ def get_config():
     cfg = VersioneerConfig()
     cfg.VCS = "git"
     cfg.style = "pep440-pre"
-    cfg.tag_prefix = ""
+    cfg.tag_prefix = "v"
     cfg.parentdir_prefix = "brian2cuda-"
     cfg.versionfile_source = "brian2cuda/_version.py"
     cfg.verbose = False
