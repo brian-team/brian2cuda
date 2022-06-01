7a3c6f48b3a57ca547b15fd2e05a9aac04a0799d
Denis Alevi <mail@denisalevi.de>
2022-06-01 17:30:55 +0200

    Add spikemonitor attribute test

--- brian2cuda/tests/test_monitor.py
+++ brian2cuda/tests/test_monitor.py
@@ -20,6 +20,37 @@ def test_state_monitor_more_threads_than_single_block():
         assert_equal(mon.v[:, t], v_init)
 
 
+@pytest.mark.standalone_compatible
+def test_spike_monitor_subgroups_all_attr():
+    G = NeuronGroup(6, '''do_spike : boolean''', threshold='do_spike')
+    G.do_spike = [True, False, False, False, True, True]
+    spikes_all = SpikeMonitor(G)
+    spikes_1 = SpikeMonitor(G[:2])
+    spikes_2 = SpikeMonitor(G[2:4])
+    spikes_3 = SpikeMonitor(G[4:])
+
+    run(defaultclock.dt)
+
+    assert_allclose(spikes_all.i, [0, 4, 5])
+    assert_allclose(spikes_all.t, [0, 0, 0]*ms)
+    assert_allclose(spikes_all.count, [1, 0, 0, 0, 1, 1])
+    assert_allclose(spikes_all.N, 3)
+
+    assert_allclose(spikes_1.i, [0])
+    assert_allclose(spikes_1.t, [0]*ms)
+    assert_allclose(spikes_1.count, [1, 0])
+    assert_allclose(spikes_1.N, 2)
+
+    assert len(spikes_2.i) == 0
+    assert len(spikes_2.t) == 0
+    assert_allclose(spikes_2.count, [0, 0])
+    assert spikes_2.N == 0
+
+    assert_allclose(spikes_3.i, [0, 1])  # recorded spike indices are relative
+    assert_allclose(spikes_3.t, [0, 0] * ms)
+    assert_allclose(spikes_1.count, [1, 1])
+    assert_allclose(spikes_1.N, 2)
+
 if __name__ == '__main__':
     import brian2cuda
     set_device('cuda_standalone', directory=None)
