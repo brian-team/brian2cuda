55c6bb0db30b8a05becfbd01e7a11a627fb7d0bf
weiyuandu <weiyuan.gin@gmail.com>
2026-09-10 16:17:33 +0200

    remove array_basename and detect dynamic arrays by prefix

--- brian2cuda/device.py
+++ brian2cuda/device.py
@@ -38,7 +38,6 @@ from brian2cuda.utils.logger import report_issue_message
 from .codeobject import (
     CUDAStandaloneCodeObject,
     CUDAStandaloneAtomicsCodeObject,
-    array_basename,
 )
 
 
@@ -552,8 +551,7 @@ class CUDAStandaloneDevice(CPPStandaloneDevice):
                 run_counter += 1
             elif func=='set_by_constant':
                 arrayname, value, is_dynamic = args
-                short = array_basename(arrayname)
-                if is_dynamic and short is not None:
+                if is_dynamic and arrayname.startswith('_dynamic_array_'):
                     host_ptr = f'{arrayname}.data()'
                     size_str = f'{arrayname}.size()'
                     # data() is void*; cudaMemcpy accepts it without a typed cast.
@@ -585,8 +583,7 @@ class CUDAStandaloneDevice(CPPStandaloneDevice):
                 main_lines.extend(stripped_deindented_lines(code))
             elif func=='set_by_single_value':
                 arrayname, item, value = args
-                short = array_basename(arrayname)
-                if short is not None:
+                if arrayname.startswith('_dynamic_array_'):
                     host_ptr = f'{arrayname}.data()'
                     # void* + offset is illegal; advance by element size in bytes.
                     dest_expr = (
@@ -614,8 +611,7 @@ class CUDAStandaloneDevice(CPPStandaloneDevice):
                 main_lines.extend(stripped_deindented_lines(code))
             elif func=='set_by_array':
                 arrayname, staticarrayname, is_dynamic = args
-                short = array_basename(arrayname)
-                if is_dynamic and short is not None:
+                if is_dynamic and arrayname.startswith('_dynamic_array_'):
                     host_ptr = f'{arrayname}.data()'
                     size_str = f'{arrayname}.size()'
                     # data() is void*; cudaMemcpy accepts it without a typed cast.
@@ -644,8 +640,7 @@ class CUDAStandaloneDevice(CPPStandaloneDevice):
                 main_lines.extend(stripped_deindented_lines(code))
             elif func=='set_array_by_array':
                 arrayname, staticarrayname_index, staticarrayname_value = args
-                short = array_basename(arrayname)
-                if short is not None:
+                if arrayname.startswith('_dynamic_array_'):
                     host_ptr = f'{arrayname}.data()'
                     dev_ptr = f'dev{arrayname}.data()'
                     memcpy_size = f'{arrayname}.size()'
